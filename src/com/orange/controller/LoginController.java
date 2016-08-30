c661a6e10d2b740c3e176242e7eb53f658b8174f
xieqp <xieqiupeng@163.com>
2016-08-30 20:46:49 +0800

    spring mvc init
    Change-Id: I37ff4d7c31d53c651061f675e0805db5b6f868b6
    Signed-off-by: xieqp <xieqiupeng@163.com>

---
+++ src/com/orange/controller/LoginController.java
@@ -0,0 +1,31 @@
+/**
+ * @(#) LoginController.java 2016年8月17日 下午8:29:28
+ * Copyright 2016 ORANGE Software Co., Ltd. All Rights Reserved
+ */
+package com.orange.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * <code>LoginController</code>类
+ * @author xieqp
+ * @version 1.0
+ * @since jdk7
+ */
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/greeting")  
+    public ModelAndView greeting(@RequestParam(value="name", defaultValue="World") String name) {  
+         Map<String, Object> map = new HashMap<String, Object>();  
+         map.put("userName", name);  
+         return new ModelAndView("/hello",map);  
+    }
+}
